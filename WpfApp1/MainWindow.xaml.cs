ed15e7e213abbd5be063e4add6d7946596caf5e5
Anjali Sheel <anjalisheel@microsoft.com>
2021-07-20 17:30:34 +0530

    Provided fix for date picker bug #1354281

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+        private void date_DateValidationError(object sender, DatePickerDateValidationErrorEventArgs e)
+        {
+            ErrorTextBlock.Visibility = Visibility.Visible;
+            ErrorTextBlock.Text = "Date got reverted to previous value.";
+
+            var automationPeer = UIElementAutomationPeer.CreatePeerForElement(ErrorTextBlock);
+            if (automationPeer != null)
+            {
+                automationPeer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+            }
+
+            automationPeer?.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+        }
+
+        private void date_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+            if (ErrorTextBlock != null)
+            {
+                ErrorTextBlock.Text = string.Empty;
+                ErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void date_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ErrorTextBlock != null)
+            {
+                ErrorTextBlock.Text = string.Empty;
+                ErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            this.username = "Ravi";
+            this.Date = DateTime.Now;
+            this.ageGroups = new List<string> { "1-10", "11-20", "20+" };
+        }
+
+        private string username;
+        public string UserName
+        {
+            get => this.username;
+            set
+            {
+                this.username = value;
+                NotifyPropertyChanged(nameof(UserName));
+            }
+        }
+
+        private List<string> ageGroups;
+        public List<string> AgeGroups
+        {
+            get => this.ageGroups;
+            set
+            {
+                this.ageGroups = value;
+                NotifyPropertyChanged(nameof(AgeGroups));
+            }
+        }
+
+        private DateTime? date;
+        public DateTime? Date
+        {
+            get => this.date;
+
+            set
+            {
+                this.date = value;
+                NotifyPropertyChanged(nameof(Date));
+            }
+        }
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                this.PropertyChanged(this, args);
+            }
+        }
+    }
+}
